413f811a776ae5e6d21bd70a7a85993c5106aff4
will <was51@pitt.edu>
2022-07-12 01:17:25 -0400

    added flask application

--- pp.py
+++ pp.py
@@ -5,7 +5,7 @@ import requests
 import json
 from dotenv import load_dotenv
 import os
-from flask import Flask, render_template, Markup
+from flask import Flask, render_template
 
 load_dotenv()
 
@@ -27,7 +27,7 @@ def get_token():
 
 
 def get_best_scores(id, token):
-    best_scores_url = f"https://osu.ppy.sh/api/v2/users/{id}/scores/best?limit=5"
+    best_scores_url = f"https://osu.ppy.sh/api/v2/users/{id}/scores/best?limit=1"
 
     response = requests.get(
         best_scores_url, headers={"Authorization": f"Bearer {token['access_token']}"}
@@ -49,6 +49,7 @@ def get_recent_score(id, token):
         print(f"Player ID {id} has not submitted a play in the last 24 hours!")
 
     return response.json()
+
 
 def get_user_img(txt):
     for play in txt:
@@ -107,32 +108,29 @@ def get_play_info(txt):
             mods = ", ".join(dataset[5])
 
         return_string += (
-            f"{dataset[0]} - {dataset[1]}<br>"
-            f"{dataset[2]}, {(dataset[3]*100):.2f}%, {dataset[4]:.1f}pp, {dataset[8]}<br>"
-            f"{mods}, {dataset[7]:,}x, Score: {dataset[6]:,}<br>"
-            f"AR {dataset[9]}, {dataset[10]} bpm, {dataset[11]} Stars<br>"
+            f"{dataset[0]} - {dataset[1]}\n"
+            f"{dataset[2]}, {(dataset[3]*100):.2f}%, {dataset[4]:.1f}pp, {dataset[8]}\n"
+            f"{mods}, {dataset[7]:,}x, Score: {dataset[6]:,}\n"
+            f"AR {dataset[9]}, {dataset[10]} bpm, {dataset[11]} Stars\n"
         )
-
     return return_string
 
-#Get info
-token = get_token()
-most_recent = get_recent_score(7429544, token)
-top_plays = get_best_scores(7429544, token)
 
 app = Flask(__name__)
 
 
-
 @app.route("/")
 def home():
-    formatted_recent = get_play_info(most_recent)
-    user_img = get_user_img(most_recent)
-    beatmap_card = get_beatmap_img(most_recent)
-    return render_template("index.html", formatted_recent = Markup(formatted_recent), user_img = user_img, beatmap_card = beatmap_card)
+    token = get_token()
+    most_recent = get_recent_score(7429544, token)  # get_best_scores(7429544, token)
 
+    # user_img = get_user_img(most_recent)
+    # beatmap_card = get_beatmap_img(most_recent)
+
+    return render_template("index.html", most_recent=most_recent)
+    # return render_template("index.html", most_recent = most_recent)
 
 
 if __name__ == "__main__":
     app.debug = True
-    app.run()   
+    app.run()
