e9ad02ec8a6c1dc278f5d8db9062b373406cc422
will <was51@pitt.edu>
2022-07-15 02:56:07 -0400

    Cleaned up display

--- pp.py
+++ pp.py
@@ -50,79 +50,13 @@ def get_recent_score(id, token):
 
     return response.json()
 
-
-def get_user_img(txt):
-    for play in txt:
-        user_img = play["user"]["avatar_url"]
-
-    return user_img
-
-
-# Passes in json formatted repsonse
-# returns id of beatmap
-def get_beatmap_img(txt):
-    for play in txt:
-        beatmapset_ids = play["beatmapset"]["covers"]["card@2x"]
-
-    return beatmapset_ids
-
-
-def get_play_info(txt):
-    submitted_data = []
-
-    for item in txt:
-        item_stats = []
-        item_stats.append(item["beatmapset"]["title"])
-        item_stats.append(item["beatmapset"]["artist"])
-
-        item_stats.append(item["rank"])
-        item_stats.append(item["accuracy"])
-
-        if item["pp"] is None:
-            item_stats.append(0)
-        else:
-            item_stats.append(item["pp"])
-
-        item_stats.append(item["mods"])
-        item_stats.append(item["score"])
-        item_stats.append(item["max_combo"])
-        stats_formatted = f"{item['statistics']['count_300'] + item['statistics']['count_geki']}/{item['statistics']['count_100'] + item['statistics']['count_katu']}/{item['statistics']['count_50']}/{item['statistics']['count_miss']}"
-        item_stats.append(stats_formatted)
-
-        item_stats.append(item["beatmap"]["ar"])
-        item_stats.append(item["beatmap"]["bpm"])
-        item_stats.append(item["beatmap"]["difficulty_rating"])
-
-        item_stats.append(item["beatmapset"]["covers"]["cover@2x"])
-
-        submitted_data.append(item_stats)
-
-        # if the flag is true, we also want to print, otherwise ignore
-
-    return_string = ""
-
-    for dataset in submitted_data:
-        if dataset[5] == []:
-            mods = "No Mod"
-        else:
-            mods = ", ".join(dataset[5])
-
-        return_string += (
-            f"{dataset[0]} - {dataset[1]}\n"
-            f"{dataset[2]}, {(dataset[3]*100):.2f}%, {dataset[4]:.1f}pp, {dataset[8]}\n"
-            f"{mods}, {dataset[7]:,}x, Score: {dataset[6]:,}\n"
-            f"AR {dataset[9]}, {dataset[10]} bpm, {dataset[11]} Stars\n"
-        )
-    return return_string
-
-
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
     token = get_token()
-    most_recent = get_recent_score(7429544, token)  # get_best_scores(7429544, token)
+    most_recent = get_best_scores(7429544, token)  # get_recent_score(7429544, token)
 
     # user_img = get_user_img(most_recent)
     # beatmap_card = get_beatmap_img(most_recent)
